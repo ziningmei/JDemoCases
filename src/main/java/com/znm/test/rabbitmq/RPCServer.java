3b5a17d3a59da023ad8d1a8eeed15f2fc102ae84
jiang.qi <jiang.qi@Ctrip.com>
2016-08-01 18:18:49 +0800

    finish rabbitmq demo

---
+++ src/main/java/com/znm/test/rabbitmq/RPCServer.java
@@ -0,0 +1,75 @@
+package com.znm.test.rabbitmq;
+
+import com.rabbitmq.client.*;
+
+/**
+ * Created by jiang.qi on 2016/8/1.
+ */
+public class RPCServer {
+
+    private static final String RPC_QUEUE_NAME = "rpc_queue";
+
+    private static int fib(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    public static void main(String[] argv) {
+        Connection
+                connection = null;
+        Channel channel = null;
+        try {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost("localhost");
+
+            connection = factory.newConnection();
+            channel = connection.createChannel();
+
+            channel.queueDeclare(RPC_QUEUE_NAME, false, false, false, null);
+
+            channel.basicQos(1);
+
+            QueueingConsumer consumer = new QueueingConsumer(channel);
+            channel.basicConsume(RPC_QUEUE_NAME, false, consumer);
+
+            System.out.println(" [x] Awaiting RPC requests");
+
+            while (true) {
+                String response = null;
+
+                QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+
+                AMQP.BasicProperties props = delivery.getProperties();
+                AMQP.BasicProperties replyProps = new AMQP.BasicProperties().builder()
+                        .correlationId(props.getCorrelationId())
+                        .build();
+
+                try {
+                    String message = new String(delivery.getBody(), "UTF-8");
+                    int n = Integer.parseInt(message);
+
+                    System.out.println(" [.] fib(" + message + ")");
+                    response = "" + fib(n);
+                } catch (Exception e) {
+                    System.out.println(" [.] " + e.toString());
+                    response = "";
+                } finally {
+                    channel.basicPublish("", props.getReplyTo(), replyProps, response.getBytes("UTF-8"));
+
+                    channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (Exception ignore) {
+                }
+            }
+        }
+    }
+
+}
